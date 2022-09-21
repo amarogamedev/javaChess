2c50ef8716a721bf65a84ea3ec81fa6747e9cc29
amarogamedev <luisfellipeamaro@gmail.com>
2022-09-21 09:59:09 -0300

    Fixed verification for valid position

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
     private int row;
 
     public ChessPosition(char column, int row) {
-        if(column < 'a' || column > 'h' || row > 1 || row > 8)
+        if(column < 'a' || column > 'h' || row < 1 || row > 8)
         {
             throw new ChessException("Invalid position");
         }
